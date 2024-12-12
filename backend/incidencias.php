0915bc4e35c0f34177074674a235f37a56ad0a87
Marc0sUM <marcosugaldemo@gmail.com>
2024-12-12 11:50:34 -0600

    Funcionalidad dinamica de las incidencias trayendo datos y guardandolos en el backend

---
+++ backend/incidencias.php
@@ -0,0 +1,219 @@
+<?php
+
+require 'db.php';
+
+function obtenerIncidencias($id_incidencias = null)
+{
+    global $pdo;
+    try {
+        if ($id_incidencias) {
+            $sql = "SELECT * FROM incidencias WHERE id_incidencias = :id_incidencias";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                'id_incidencias' => $id_incidencias
+            ]);
+        } else {
+            $sql = "SELECT * FROM incidencias";
+            $stmt = $pdo->query($sql); 
+        }
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (\Throwable $th) {
+        logError("Error al obtener incidencias: " . $th->getMessage());
+        return [];
+    }
+}
+
+function obtenerUsuarios(){
+    global $pdo;
+
+    try {
+        $sql = "SELECT id_usuario, userName FROM usuarios";
+        $stmt = $pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (\Throwable $th) {
+        logError("Error al obtener prioridades: " . $th->getMessage());
+        return [];
+    }
+}
+
+function obtenerPrioridades()
+{
+    global $pdo;
+
+    try {
+        $sql = "SELECT id_prioridad, nombre, color FROM Prioridad";
+        $stmt = $pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (\Throwable $th) {
+        logError("Error al obtener prioridades: " . $th->getMessage());
+        return [];
+    }
+}
+
+function obtenerStatus()
+{
+    global $pdo;
+
+    try {
+        $sql = "SELECT id_status, nombre FROM Status";
+        $stmt = $pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (\Throwable $th) {
+        logError("Error al obtener status: " . $th->getMessage());
+        return [];
+    }
+}
+
+function crearIncidencia($id_usuario, $nombre, $descripcion,$id_status, $id_prioridad)
+{
+
+    global $pdo;
+
+    try {
+        $sql = "INSERT INTO incidencias (id_usuario, nombre, descripcion, id_status, id_prioridad) 
+        values (:id_usuario, :nombre, :descripcion,:id_status, :id_prioridad)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            'id_usuario' => $id_usuario,
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+            'id_status' => $id_status,
+            'id_prioridad' => $id_prioridad
+        ]);
+        return $pdo->lastInsertId();
+    } catch (\Throwable $th) {
+        logError("Error al crear incidencia: " . $th->getMessage());
+        return false;
+    }
+}
+
+function editarIncidencia($id_usuario, $nombre, $descripcion, $id_status, $id_prioridad)
+{
+    global $pdo;
+    try {
+        $sql = "UPDATE incidencias set nombre = :nombre, descripcion = :descripcion, id_status = :id_status, id_prioridad = :id_prioridad WHERE id_usuario = :id_usuario AND id_incidencias = :id_incidencias";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            'id_usuario' => $id_usuario,
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+            'id_status' => $id_status,
+            'id_prioridad' => $id_prioridad
+        ]);
+        $affected_rows = $stmt->rowCount();
+        return $affected_rows > 0;
+    } catch (\Throwable $th) {
+        logError("Error al editar incidencia: " . $th->getMessage());
+        return false;
+    }
+}
+
+function eliminarIncidencia($id_incidencia)
+{
+
+    global $pdo;
+
+    try {
+        $sql = "DELETE FROM incidencias WHERE id_incidencias = :id_incidencias";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(([
+            'id_incidencias' => $id_incidencia
+        ]));
+        return $stmt->rowCount() > 0;
+    } catch (\Throwable $th) {
+        logError("Error al eliminar incidencia: " . $th->getMessage());
+        return false;
+    }
+}
+
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json');
+
+function getJsonInput()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+session_start();
+if (isset($_SESSION['user_id'])) {
+
+    $user_id = $_SESSION['user_id'];
+    logDebug("Usuario logeado: $user_id");
+    $input = getJsonInput();
+    switch ($method) {
+        case 'POST':
+            
+            if (isset($input['nombre'], $input['descripcion'], $input['id_status'], $input['id_prioridad'])) {
+              
+                $id_incidencia = crearIncidencia($user_id, $input['nombre'], $input['descripcion'],  $input['id_status'], $input['id_prioridad']);
+                if ($id_incidencia > 0) {
+               
+                    http_response_code(201);
+                    echo json_encode(['id_incidencia' => $id_incidencia]);
+                } else {
+                
+                    http_response_code(500);
+                    echo json_encode(['message' => 'Error al crear incidencia']);
+                }
+            } else {
+                http_response_code(400);
+                echo json_encode(['message' => 'Faltan datos']);
+            }
+            break;
+        case 'GET':
+            $prioridades = obtenerPrioridades();
+            $status = obtenerStatus();
+            $usuario = obtenerUsuarios();
+            if (isset($input['id_incidencias']))  {
+                $id_incidencias = $_GET['id_incidencias'];
+                $incidencias = obtenerIncidencias($id_incidencias); 
+            } else {
+                $incidencias = obtenerIncidencias();
+            }
+            echo json_encode([
+                'prioridades' => $prioridades,
+                'incidencias' => $incidencias,
+                'status' => $status,
+                'usuarios' => $usuario
+            ]);
+            break;
+        case 'PUT':
+            if (isset($input['id_incidencias'], $input['nombre'], $input['descripcion'], $input['id_status'], $input['id_prioridad'])) {
+                $actualizado = editarIncidencia($input['id_incidencias'], $input['nombre'], $input['descripcion'],  $input['id_status'], $input['id_prioridad']);
+                if ($actualizado) {
+                    http_response_code(200);
+                    echo json_encode(['message' => 'Incidencia actualizada correctamente']);
+                } else {
+                    http_response_code(404);
+                    echo json_encode(['message' => 'Incidencia no encontrada']);
+                }
+            } else {
+                http_response_code(400);
+                echo json_encode(['message' => 'Faltan datos']);
+            }
+            break;
+        case 'DELETE':
+            if (isset($_GET['id_incidencias'])) {
+                $eliminado = eliminarIncidencia($_GET['id_incidencias']);
+                if ($eliminado) {
+                    http_response_code(200);
+                    echo json_encode(['message' => 'Incidencia eliminada correctamente']);
+                } else {
+                    http_response_code(404);
+                    echo json_encode(['message' => 'Incidencia no encontrada']);
+                }
+            } else {
+                http_response_code(400);
+                echo json_encode(['message' => 'Faltan datos']);
+            }
+            break;
+        default:
+            http_response_code(405);
+            echo json_encode(['message' => 'Metodo no permitido']);
+            break;
+    }
+} else {
+    http_response_code(401);
+    echo json_encode(['message' => 'No autorizado']);
+}
--- javaScript/incidencias.js
+++ javaScript/incidencias.js
@@ -1,77 +1,221 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const incidents = [
-        {
-            id: 1,
-            definition: "Server down",
-            priority: "P1 Critical",
-            summary: "Lost the connection",
-            impactedServices: "Web page",
-            status: "Open",
-            date: "9/11/2024"
-        },
-        {
-            id: 2,
-            definition: "Slow response time",
-            priority: "P2 High",
-            summary: "The server is responding slowly",
-            impactedServices: "Database",
-            status: "Open",
-            date: "10/11/2024"
-        }
-    ];
+document.addEventListener('DOMContentLoaded', function () {
 
-    function loadIncidents() {
+    let isEditing = false;
+    let incidents = [];
+    let priorities = [];
+    let status = [];
+    let usuarios = [];
+    let editingId;
+    const API_URL = 'backend/incidencias.php';
+
+    async function loadIncidents() {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                credentials: 'include'
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                incidents = data.incidencias;
+                priorities = data.prioridades;
+                status = data.status;
+                usuarios = data.usuarios;
+                renderIncidents(incidents);
+            } else {
+                if (response.status === 401) {
+                    window.location.href = 'index.html';
+                }
+                console.error('Error:', response.status);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function renderIncidents(incidents) {
         const incidentList = document.getElementById('incident-list');
         incidentList.innerHTML = '';
-        incidents.forEach(function(incident) {
+        incidents.forEach(incident => {
+            const priorityName = priorities.find(priority => priority.id_prioridad === incident.id_prioridad)?.nombre || 'Desconocido';
+            const priorityColor = priorities.find(priority => priority.id_prioridad === incident.id_prioridad)?.color || 'Desconocido';
+            const statusName = status.find(status => status.id_status === incident.id_status)?.nombre || 'Desconocido';
+            const userName = usuarios.find(usuario => usuario.id_usuario === incident.id_usuario)?.userName || 'Desconocido';
+            console.log(userName);
             const incidentCard = document.createElement('div');
             incidentCard.className = 'd-flex flex-column mb-2';
             incidentCard.innerHTML = `
-                <div class="card mb-3">
-                    <div class="card-body row pb-0">
-                        <div class="col-sm-1 m-1 badge text-bg-secondary "><p class="card-text ">#${incident.id}</p></div>
-                        <div class="col-sm-2"><p class="card-text"><span class="fw-bold badge bg-danger">${incident.priority}</span></p></div>
-                        <div class="col-sm-7"><h5 class="card-title">${incident.definition}</h5></div>
-                        <div class="col-sm-1 m-1 badge text-bg-info "><p class="card-text">${incident.status}</p></div>
+            <div class="card mb-3 custom-card">
+                <div class="card-body row pb-0 align-items-center">
+                    
+                    <div class="col-12 col-md-2 mb-2 mb-md-0 text-center text-md-start">
+                        <span class="badge text-bg-secondary">#${incident.id_incidencias}</span>
                     </div>
-                    <div class="card-body row p-1">   
-                        <div class="col-sm-6"><p class="card-text">${incident.impactedServices}</p></div>
-                        <div class="col-sm-6 d-flex justify-content-end"><p class="card-text"><small class="text-muted">Date: ${incident.date}</small></p></div>
+                    
+                    <div class="col-6 col-md-2 mb-2 mb-md-0 text-center">
+                        <span class="fw-bold badge bg-danger">${priorityName}</span>
+                    </div>
+
+                    <div class="col-12 col-md-5 mb-2 mb-md-0 text-center text-md-start">
+                        <h5 class="card-title mb-0">${incident.descripcion}</h5>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-2 mb-md-0 text-center">
+                        <span class="badge bg-info">${statusName}</span>
                     </div>
                 </div>
-            `;
+                <div class="card-body row p-1 align-items-center">
+
+                    <div class="col-12 col-md-6 text-center text-md-start mb-2 mb-md-0">
+                        <p class="card-text"><span class="fw-bold">${userName}</span></p>
+                    </div>
+
+                    <div class="col-12 col-md-6 text-center text-md-end">
+                        <p class="card-text"><small class="text-muted">Creation Date: ${incident.fecha_creacion}</small></p>
+                    </div>
+                </div>
+            </div>
+        `;
             incidentList.appendChild(incidentCard);
+        });
+
+
+    }
+
+    function handleEditIncident(event) {
+        try {
+            const incidentId = event.target.dataset.id;
+            const incident = incidents.find(incident => incident.id === parseInt(incidentId));
+
+            document.getElementById('name').value = incident.definition;
+            document.getElementById('priority').value = incident.priority;
+
+            isEditing = true;
+            editingId = incidentId;
+
+            const incidentModal = new bootstrap.Modal(document.getElementById('incidentModal'));
+            incidentModal.show();
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    async function handleDeleteIncident(event) {
+        const id = parseInt(event.target.dataset.id);
+        const response = await fetch(API_URL, {
+            method: 'DELETE',
+            credentials: 'include'
+        })
+        if (response.ok) {
+            loadIncidents();
+        } else {
+            console.error('Error:', response.status);
+        }
+    }
+
+    async function loadPriorities() {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if (response.ok) {
+                const data = await response.json();
+                insertPriorities(data.prioridades);
+            } else {
+                console.error('Error:', response.status);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function insertPriorities(priorities) {
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+        dropdownMenu.innerHTML = '';
+
+        priorities.forEach(priority => {
+            const dropdownItem = document.createElement('li');
+            const dropdownLink = document.createElement('a');
+            dropdownLink.classList.add('dropdown-item');
+            dropdownLink.textContent = `P${priority.id_prioridad} ` + priority.nombre;
+            dropdownLink.dataset.id = priority.id_prioridad;
+            dropdownLink.addEventListener('click', (event) => {
+                selectPriority(event, priority);
+            });
+
+            dropdownItem.appendChild(dropdownLink);
+            dropdownMenu.appendChild(dropdownItem);
         });
     }
 
-    document.getElementById('incident-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        
-        const definition = document.getElementById("definition").value;
-        const priority = document.getElementById("priority").value;
-        const summary = document.getElementById("summary").value;
-        const impactedServices = document.getElementById("impacted-services").value;
+    function selectPriority(event, priority) {
+        const priorityButton = document.getElementById('priorityDropdown');
+        priorityButton.textContent = priority.nombre;
+        priorityButton.dataset.priorityId = priority.id_prioridad;
+    }
 
-        const newIncident = {
-            id: incidents.length + 1,
-            definition: definition,
-            priority: priority,
-            summary: summary,
-            impactedServices: impactedServices,
-            status: "Open",
-            date: new Date().toLocaleDateString() // fecha actual
-        };
-        
-        incidents.push(newIncident);
+    document.getElementById('incident-form').addEventListener('submit', async function (event) {
+        event.preventDefault();
 
+        const name = document.getElementById('name').value;
+        const priorityId = parseInt(document.getElementById('priorityDropdown').dataset.priorityId);
+        const description = document.getElementById('description').value;
+        const status = 2;
+
+        if (isEditing) {
+            const response = await fetch(`${API_URL}?id=${edittingId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: name,
+                    descripcion: description,
+                    id_status: status,
+                    id_prioridad: priorityId,
+                }),
+                credentials: 'include'
+            });
+            if (!response.ok) {
+                console.error('Error trying to save a task');
+            }
+        } else {
+            const newTask = {
+                nombre: name,
+                descripcion: description,
+                id_status: status,
+                id_prioridad: priorityId,
+            };
+            console.log(newTask);
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask),
+                credentials: 'include'
+            });
+            if (!response.ok) {
+                console.error('Error trying to save a task');
+            }
+        }
         const modal = bootstrap.Modal.getInstance(document.getElementById('incidentModal'));
         modal.hide();
         loadIncidents();
+    })
+
+    document.getElementById('incidentModal').addEventListener('show.bs.modal', function () {
+        if (!isEditing) {
+            document.getElementById('incident-form').reset();
+        }
     });
 
-    document.getElementById('incidentModal').addEventListener('show.bs.modal', function() {
-        document.getElementById('incident-form').reset();
-    });
-
+    document.getElementById("incidentModal").addEventListener('hidden.bs.modal', function () {
+        editingId = null;
+        isEditing = false;
+    })
+    loadPriorities();
     loadIncidents();
 });
